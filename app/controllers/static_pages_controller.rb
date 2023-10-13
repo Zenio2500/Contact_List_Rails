2672980283c55148c22ad379079347de91ed65ce
Mister Z <zenio2500@gmail.com>
2023-10-13 13:11:21 -0300

    Páginas Estáticas

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,5 @@
 class StaticPagesController < ApplicationController
+    def index
+        @nome="Zenio"
+    end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,6 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
-  def index
-    @nome="Zenio"
-  end
   # test "the truth" do
   #   assert true
   # end
