0cab8e3e75a0d675ce619bfbe477420bbc764d42
Mister Z <zenio2500@gmail.com>
2023-10-12 21:16:05 -0300

    Hello World

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def index
+    @nome="Zenio"
+  end
+  # test "the truth" do
+  #   assert true
+  # end
+end
